03c5b0b95f5e56d45c4c761b996636a676058438
JaewonKim <huhu420@naver.com>
2020-06-15 14:49:07 +0900

    [delete] Remove unrelated projects
    - Remove unrelated projects excepts rabbitmq-*
    - Thanks to Hellozin

--- jpa-specification/src/main/java/me/hellozin/study/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package me.hellozin.study;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificationExecutor<Post> {
-}
